9b9faacf2ecfeadfb7ea7d2c56992fd1b5923a33
wangbin@yingxingroup.net <wangbin625>
2021-02-26 12:43:52 +0800

    order by

--- src/main/java/com/sihabeer/mapper/ProduceTaskMapper.java
+++ src/main/java/com/sihabeer/mapper/ProduceTaskMapper.java
@@ -11,7 +11,7 @@ public interface ProduceTaskMapper {
     @Insert("insert into producetask(id,goods_name,goods_type,produce_num,produce_daily,start_time,end_time,status,principal) " +
             "values(#{id},#{goodsName},#{goodsType},#{produceNum},#{produceDaily},#{startTime},#{endTime},#{status},#{principal})")
     int insertProduceTask(ProduceTask produceTask);
-    @Select("select * from producetask")
+    @Select("select * from producetask order by start_time desc")
     List<ProduceTask> listProduceTask();
     @Delete("delete from producetask where id=#{id}")
     int deleteProducetask(String id);
